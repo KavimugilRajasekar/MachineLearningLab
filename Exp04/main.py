95e6716c3960ac0a6ea0f276a2d5152c0a5d6fc4
KavimugilRajasekar <kavimugil28@gmail.com>
2026-01-07 14:51:07 +0530

    Package Exercise Done .

---
+++ Exp04/main.py
@@ -0,0 +1,9 @@
+from package import calculator, greetings
+
+print(calculator.multiply(4, 5))
+print(greetings.say_hello("Kavi"))
+import math
+import datetime
+
+print(math.sqrt(16))
+print(datetime.date.today())
