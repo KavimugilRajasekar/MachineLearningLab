31ef2c5d4d4cadb32ee825d22e237d40e8892efa
KavimugilRajasekar <kavimugil28@gmail.com>
2026-01-07 15:00:01 +0530

    Organizing my Directory Structure

--- Exp04/main.py
+++
@@ -1,9 +0,0 @@
-from package import calculator, greetings
-
-print(calculator.multiply(4, 5))
-print(greetings.say_hello("Kavi"))
-import math
-import datetime
-
-print(math.sqrt(16))
-print(datetime.date.today())
